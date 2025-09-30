c93bb6d8cb6076fface17e75c98e67176560908a
Phan Thanh Thảo <phanthanhthao892005@gmail.com>
2025-09-30 22:10:47 +0700

    Add files via upload

---
+++ 26_PhanThanhThao_BVN4.3.py
@@ -0,0 +1,22 @@
+def mahoa_rsa(p, q, e, plaintext):
+    n = p * q
+    phi_n = (p - 1) * (q - 1)
+    print(f"n = {n}, phi(n) = {phi_n}")
+
+    public_key = (e, n)
+
+    ma = [(pow(ord(tu), e, n)) for tu in plaintext]
+
+    return ma, public_key
+
+
+#Test
+p = 17
+q = 23
+e = 5
+plaintext = "PhanThanhThao"
+ma, public_key = mahoa_rsa(p, q, e, plaintext)
+
+print("Khoa cong khai (e, n):", public_key)
+print("Noi dung ban dau:", plaintext)
+print("Ma hoa:", ma)
